4c749069e6d9bf58398a8f55c9d137d32bb11ba1
Jacob Blakely <jacob@codekirei.com>
2016-02-23 13:28:41 -0800

    add lots of deps to package.json

---
+++ bin/dev-server.js
@@ -0,0 +1,57 @@
+'use strict'
+
+// import
+//----------------------------------------------------------
+const devMiddleware = require('webpack-dev-middleware')
+const path = require('path')
+const sync = require('browser-sync').create()
+const webpack = require('webpack')
+const HtmlPlugin = require('html-webpack-plugin')
+
+// top level vars
+//----------------------------------------------------------
+const cwd = process.cwd()
+const publicPath = '/'
+
+// webpack
+//----------------------------------------------------------
+const bundler = webpack({
+  devtool: 'cheap-module-eval-source-map',
+  entry: path.resolve(cwd, 'src', 'scripts', 'app.jsx'),
+  output: {
+    path: publicPath,
+    filename: 'app.js',
+    publicPath,
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel',
+        test: /\.jsx?$/,
+        include: path.join(cwd, 'src', 'scripts'),
+        query: {
+          presets: ['react', 'es2015'],
+        }
+      },
+    ]
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: path.join('src', 'markup', 'index.html'),
+    })
+  ]
+})
+
+// browser-sync
+//----------------------------------------------------------
+sync.init({
+  open: false,
+  server: publicPath,
+  middleware: devMiddleware(bundler, {
+    publicPath,
+    noInfo: true,
+  }),
+  files: [
+    path.join('src', '**', '*')
+  ]
+})
